ae583c84e261cb0dd70a134dfd6b709f2bc67d68
Kaisle <mollardk@gmail.com>
2024-03-03 17:29:05 -0800

    GoogleMap

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,58 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 const CLIENT_ID = 'AIzaSyBWLPQeL_3lvbQF1Aibna2oyUNt7yz2oNg';
 
-// Map Constructor
-//google.maps.Map(mapDiv[, opts])
-
-// Marker
-// google.maps.Marker([opts])
-
 function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${CLIENT_ID}&libraries=maps`;
+    script.defer = true;
+    script.async = true;
+
+    // Use a boolean flag to track whether the Google Maps script has loaded
+    let isGoogleMapsScriptLoaded = false;
+
+    // Define a callback function for the script's onload event
+    script.onload = () => {
+      isGoogleMapsScriptLoaded = true;
+    };
+
+    document.head.appendChild(script);
+
+    // Check for the Google Maps script every 100 milliseconds
+    const checkGoogleMapsScript = setInterval(() => {
+      if (isGoogleMapsScriptLoaded) {
+        clearInterval(checkGoogleMapsScript);
+        
+        // The Google Maps script has loaded, call the initMap function
+        window.initMap = () => {
+          const map = new window.google.maps.Map(document.getElementById('map'), {
+            center: { lat: 49.27964782714844, lng: -122.91876220703125 },
+            zoom: 14,
+          });
+
+          const marker = new window.google.maps.Marker({
+            position: { lat: 49.27964782714844, lng: -122.91876220703125 },
+            map: map,
+            title: 'My location',
+          });
+        };
+
+        // Call the initMap function
+        window.initMap();
+      }
+    }, 100);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="map" style={{ height: '400px', width: '100%' }}></div>
       </header>
     </div>
   );
